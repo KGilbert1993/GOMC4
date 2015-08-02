2430597d24826eab4a377a4e3e2900df7e52a2f9
Kevin Gilbert <k.gilbert.ch@gmail.com>
2015-08-02 13:33:43 -0500

    Added buttons.py back into _UIModule

---
+++ dev2/_UIModule/buttons.py
@@ -0,0 +1,46 @@
+import pygame
+from pygame.locals import *
+import os, sys
+
+green = (0,200,0, 50)
+red = (200,0,0, 50)
+grey = (160,160,160,70)
+
+def test():
+    print 'test worked!'
+
+class Button(object):
+    width = 0
+    height = 0
+    x = 0
+    y = 0
+    img = None
+    window = None
+    onclick_func = None
+
+    def __init__(self, window, w, h, x, y, image):  # TODO switch w,h,x,y to x,y,w,h to match pygame rect convention
+        self.width = w
+        self.height = h
+        self.x = x
+        self.y = y
+        self.window = window
+        print os.path.dirname(os.path.realpath(__file__))
+        self.img = pygame.image.load(image)
+        self.img = pygame.transform.scale(self.img, (self.width, self.height))
+        self.window.blit(self.img, (self.x, self.y))
+
+    def draw(self):
+        pygame.draw.rect(self.window, green, (self.x-1, self.y-1, self.width+1, self.height+1), 4)
+        self.window.blit(self.img, (self.x, self.y))
+
+    def attach_callback(self, func):
+        print 'attaching callback'
+        self.onclick_func = func
+
+    def handle_event(self, event):
+        print 'on clicking'
+        self.onclick_func()
+
+
+class Palette(object):
+    buttons = []
